c06fff21845982d18c441c6217bf86ae3b0e047f
SunnyShekhar-code <sunnyshekhar1729@gmail.com>
2026-07-17 08:12:06 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/in/strike/SpringbootCrud/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package in.strike.SpringbootCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
